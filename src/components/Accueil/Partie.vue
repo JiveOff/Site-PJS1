c9cfdca92faaae1050e086fc88e35c875ccddb22
JiveOff <antoine@jiveoff.fr>
2020-11-19 11:18:10 +0100

    background etc

--- src/components/Accueil/Partie.vue
+++ src/components/Accueil/Partie.vue
@@ -5,7 +5,7 @@
       <div class="content">
         <h2 class="major">{{ cat.title }}</h2>
         <p>{{ cat.description }}</p>
-        <a @click="$router.push({ path: 'article/1' })" class="special">Accéder à la categorie</a>
+        <a @click="smoothPageSwitch({ path: 'article/1' });" class="special">Accéder à la categorie</a>
       </div>
     </div>
   </section>
@@ -14,7 +14,13 @@
 <script>
 export default {
   name: "Partie",
-  props: ['cat']
+  props: ['cat'],
+  methods: {
+    smoothPageSwitch(obj) {
+      window.scrollTo({ top: 0, behavior: 'smooth' })
+      this.$router.push(obj);
+    }
+  }
 }
 </script>
 
